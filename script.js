499639865d87fac24451e4261a700c2a174123f7
alyssapiscitelli <122324506+alyssapiscitelli@users.noreply.github.com>
2023-02-06 18:44:39 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log ("I am having a lot of fun in the class I am enjoying learning everything in visual studio code");
+var firstName (Alyssa); 
+var lastName (Piscitelli);
+alert ('My name is Alyssa Piscitelli');
+var myAge(31 years old);
+alert(I am + myAge);
